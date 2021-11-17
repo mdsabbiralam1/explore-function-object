dd10f08d8a9cd5c5571ca806b5dbfe1f8a8dbfa7
mdsabbiralam <mdsabbiralam009@gmail.com>
2021-11-18 02:52:53 +0600

    added recap all code

--- recap.js
+++ recap.js
@@ -8,6 +8,7 @@ console.log(bookList);
 */
 
 // recap 17-11-2021
+// variableg
 var myFavouritBook = ['social science'];
 var bookList = ['math', 'chemistry', 'science', 'biology'];
 var bookIndex = bookList.indexOf('chemistry');
@@ -26,3 +27,24 @@ console.log(bookList);
 // pop
 bookList.pop();
 console.log(bookList);
+
+// conditoin  while loop
+var mybudget = 5000;
+var mobilePrice = 10000;
+if (mybudget >= mobilePrice) {
+               console.log('mobile ta nen boss');
+}
+else {
+               console.log('ja batpar sor');
+}
+var i = 0;
+while (i < 10) {
+               console.log(i);
+               console.log('loping loping loping');
+               i++;
+}
+// condition with for loop
+for (i=0; i <= 7; i++) {
+               // console.log(i);
+}
+
