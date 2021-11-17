11dd774552842362897f077e6cf7e4df87a63067
mdsabbiralam <mdsabbiralam009@gmail.com>
2021-11-18 03:14:50 +0600

    added declaration and call function

---
+++ function.js
@@ -0,0 +1,11 @@
+// declaration the function
+
+function trunOnswitch () {
+               console.log('walk toward the switch');
+               console.log('press the switch');
+}
+// call the function
+trunOnswitch();
+console.log('mama kemon aso');
+console.log('mama ki koro ekhon');
+trunOnswitch();
