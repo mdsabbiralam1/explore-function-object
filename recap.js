ae7bb4bc2ed370c2d4102230063c4962d545bbe2
mdsabbiralam <mdsabbiralam009@gmail.com>
2021-11-18 02:23:56 +0600

    recap js

---
+++ recap.js
@@ -0,0 +1,28 @@
+/*
+var myFavouritBook = 'Biology Book';
+var bookList = ['math', 'chemistry', 'Social Science', 'Programming Assential'];
+var bookIndex = bookList.indexOf('Social Science');
+console.log(bookIndex);
+bookList[2] = 'English';
+console.log(bookList);
+*/
+
+// recap 17-11-2021
+var myFavouritBook = ['social science'];
+var bookList = ['math', 'chemistry', 'science', 'biology'];
+var bookIndex = bookList.indexOf('chemistry');
+bookList[2] =['English'];
+// console.log(bookIndex);
+// console.log(bookList);
+
+// index find 
+var secondIndex = bookList[2];
+console.log(secondIndex);
+
+// push 
+bookList.push('sabbir');
+console.log(bookList);
+
+// pop
+bookList.pop();
+console.log(bookList);
