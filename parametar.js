4a51ab281ff50c008bcad1b6b9eeb3c364ba009b
mdsabbiralam <mdsabbiralam009@gmail.com>
2021-11-19 01:51:50 +0600

    first commit

---
+++ parametar.js
@@ -0,0 +1,7 @@
+function bringSingara(taka) {
+               console.log('Singara er taka', taka);
+               console.log('mama singara den');
+}
+bringSingara(100);
+var money = 15;
+bringSingara(money)
