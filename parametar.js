2a2e44a562885629764ff3e3831c3381b0134b62
mdsabbiralam <mdsabbiralam009@gmail.com>
2021-11-24 01:40:04 +0600

    recap perameter added

--- parametar.js
+++ parametar.js
@@ -1,3 +1,4 @@
+/*
 function bringSingara(taka) {
                console.log('Singara er taka', taka);
                console.log('mama singara den');
@@ -5,3 +6,13 @@ function bringSingara(taka) {
 bringSingara(100);
 var money = 15;
 bringSingara(money)
+*/
+
+
+
+// recap
+function bringTea (taka) {
+               console.log('Tea er taka', taka);
+               console.log('mama tea den');
+}
+bringTea(100);
