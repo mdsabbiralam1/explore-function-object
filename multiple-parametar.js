7062136cc78780860b6d71235ab9e3bff040a3a2
mdsabbiralam <mdsabbiralam009@gmail.com>
2021-11-24 16:55:51 +0600

    added multiplication section

--- multiple-parametar.js
+++ multiple-parametar.js
@@ -1,3 +1,4 @@
+/*
 function addTwoNumber (number1, number2) {
                // console.log(number1) 
                console.log(number1, number2);
@@ -10,3 +11,15 @@ var secondNumber = 50;
 var result = addTwoNumber(firstNumber, secondNumber);
 
 console.log('result value:', result);
+*/
+// Multiplication 
+function multiplication (num1, num2) {
+               console.log(num1, num2);
+               var totalMark = num1 * num2;
+               return totalMark;
+               
+}
+var mathNum = 54;
+var englishNum = 60;
+var result = multiplication (mathNum, englishNum);
+console.log(result);
