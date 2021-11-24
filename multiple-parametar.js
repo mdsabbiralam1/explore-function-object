485cbe99d85a987e9d696c0164bde3f993bacc1b
mdsabbiralam <mdsabbiralam009@gmail.com>
2021-11-24 16:15:29 +0600

    added second commit

--- multiple-parametar.js
+++ multiple-parametar.js
@@ -1,4 +1,12 @@
 function addTwoNumber (number1, number2) {
                // console.log(number1) 
                console.log(number1, number2);
+               var total = number1 + number2;
+               return total;
 }
+
+var firstNumber = 31;
+var secondNumber = 50;
+var result = addTwoNumber(firstNumber, secondNumber);
+
+console.log('result value:', result);
