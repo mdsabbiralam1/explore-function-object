174ab4bf4f8a2ab150b22e50aaad3257d520a7d7
mdsabbiralam <mdsabbiralam009@gmail.com>
2021-11-24 22:14:17 +0600

    added object properties first commit

---
+++ object-properties.js
@@ -0,0 +1,9 @@
+var leptop = {
+               name: 'Hp Eletbook g3',
+               color: 'silver',
+               Storage: '156 gb',
+               proccesor: 'core i5',
+               ram: 8
+};
+// console.log(leptop);
+console.log(leptop.ram);
