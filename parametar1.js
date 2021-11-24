76eae6d6350bc09de87940660d4c576152daad0c
mdsabbiralam <mdsabbiralam009@gmail.com>
2021-11-24 15:29:16 +0600

    first commit

---
+++ parametar1.js
@@ -0,0 +1,13 @@
+function bringLeptop (taka) {
+               console.log('leptop price:', taka);
+               console.log('Hp leptop');
+               var leptopPrice = 30000;
+               var pcQuantity = myBudget / leptopPrice;
+               return pcQuantity;
+               
+}
+
+var myBudget = 60000;
+var leptop = bringLeptop(myBudget);
+
+ 
