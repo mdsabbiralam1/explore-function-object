3b618a89ad7565f812bc509f5ca63626c0f2fb15
mdsabbiralam <mdsabbiralam009@gmail.com>
2021-11-24 17:09:44 +0600

    added biyogFunction

--- multiple-parametar.js
+++ multiple-parametar.js
@@ -1,4 +1,6 @@
 /*
+
+// addition number
 function addTwoNumber (number1, number2) {
                // console.log(number1) 
                console.log(number1, number2);
@@ -13,6 +15,7 @@ var result = addTwoNumber(firstNumber, secondNumber);
 console.log('result value:', result);
 */
 // Multiplication 
+/*
 function multiplication (num1, num2) {
                console.log(num1, num2);
                var totalMark = num1 * num2;
@@ -23,3 +26,16 @@ var mathNum = 54;
 var englishNum = 60;
 var result = multiplication (mathNum, englishNum);
 console.log(result);
+*/
+
+// subtraction number
+
+function biyogFunction (nu1, nu2) {
+               console.log(nu1, nu2);
+               var returnTk = nu1 - nu2;
+               return returnTk;
+}
+var singaraPrice = 10;
+var myCash = 15;
+var returnTaka = biyogFunction (myCash, singaraPrice); 
+console.log('boss apni ferot paben :', returnTaka);
