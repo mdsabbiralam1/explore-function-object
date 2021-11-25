d5813924b2712206e7a43d1744fa04af290f4232
mdsabbiralam <mdsabbiralam009@gmail.com>
2021-11-25 19:42:36 +0600

    array break point added

--- break-continue.js
+++ break-continue.js
@@ -10,9 +10,20 @@ var i = 0;
  }
 */
 //  for loop break
+/*
 for (i = 1; i < 10; i++) {
                console.log(i);
                if (i == 6) {
                               break;
                }
- }
+ } */
+
+//  array break point
+var examMark = [41, 85, 58, 98, 65, 99, 69, 40, 29, 65];
+for (i = 0; i < examMark.length; i++) {
+               var element = examMark[i];
+               console.log(element);
+               if(i == 2) {
+                              break;
+               }
+}
