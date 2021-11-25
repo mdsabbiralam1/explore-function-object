6f9c64708752e519968b6978a6bd6e6f80056130
mdsabbiralam <mdsabbiralam009@gmail.com>
2021-11-25 17:40:17 +0600

    added while and for loop

---
+++ switch.js
@@ -0,0 +1,27 @@
+// if/ else
+var color = 'white';
+
+if (color == 'red') {
+               console.log('color is red');
+}
+else if ( color == 'yellow') {
+               console.log('color is yellow');
+}
+else if ( color == 'green') {
+               console.log('color is green');
+}
+else if (color == 'blue') {
+               console.log('color is blue');
+}
+else if (color == 'orange') {
+               console.log('color is orange');
+}
+else if (color == 'sky') {
+               console.log('color is sky');
+}
+else if (color == 'white') {
+               console.log('color is white');
+}
+else {
+               console.log('color is black');
+}
