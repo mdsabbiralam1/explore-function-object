ab9c84aadc617156e8569f903cbd2b7465faba8e
mdsabbiralam <mdsabbiralam009@gmail.com>
2021-11-25 19:19:49 +0600

    while loop break section

---
+++ break-continue.js
@@ -0,0 +1,17 @@
+// whle loop break
+var i = 0;
+ while (i < 15) {
+               console.log(i);
+               if (i == 5) {
+                              break;
+               }
+               i++;
+ }
+
+//  for loop break
+ for (i = 0, i < 10, i++) {
+               console.log(i);
+               if (i == 4) {
+                              break;
+               }
+ }
